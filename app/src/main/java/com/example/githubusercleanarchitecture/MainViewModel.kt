84f0f54caa73d14739abd220fe2d0681794d0dba
Muhammad Ilham El Hakim <m.ilhamhakim@gmail.com>
2023-10-05 20:35:29 +0700

    fixing di and clean arcitecture model on mainviewModel

--- app/src/main/java/com/example/githubusercleanarchitecture/MainViewModel.kt
+++ app/src/main/java/com/example/githubusercleanarchitecture/MainViewModel.kt
@@ -1,11 +1,9 @@
 package com.example.githubusercleanarchitecture
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import com.example.core.data.source.remote.response.ItemsItem
 import com.example.core.domain.usecase.UserUseCase
 
 class MainViewModel(private val userUseCase: UserUseCase): ViewModel(){
@@ -15,13 +13,11 @@ class MainViewModel(private val userUseCase: UserUseCase): ViewModel(){
         usernameLiveData.value = username
     }
 
-    fun getUser(): LiveData<List<ItemsItem>> {
-        return Transformations.switchMap(usernameLiveData) { username ->
-            if (username.isBlank()) {
-                userUseCase.getAllUser("").asLiveData()
-            } else {
-                userUseCase.getAllUser(username).asLiveData()
-            }
+    fun getUser() = Transformations.switchMap(usernameLiveData) { username ->
+        if (username.isBlank()) {
+            userUseCase.getAllUser("").asLiveData()
+        } else {
+            userUseCase.getAllUser(username).asLiveData()
         }
     }
 }
